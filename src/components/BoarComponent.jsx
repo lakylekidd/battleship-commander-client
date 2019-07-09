01732491074abeab1dd452c8d480417c87cc65fd
Billy Vlachos <williamcaminiti@live.com>
2019-07-09 14:34:24 +0200

    Create board component and css

---
+++ src/components/BoarComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./css/BoardComponent.css";
+
+export default class BoarComponent extends Component {
+    render() {
+        return (
+            <div className="board">
+
+            </div>
+        )
+    }
+}
