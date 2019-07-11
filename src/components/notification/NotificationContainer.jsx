5bdb2f448c5acd1950a322f8413795cb58ee64fd
Billy Vlachos <williamcaminiti@live.com>
2019-07-11 23:01:32 +0200

    Add notification component

---
+++ src/components/notification/NotificationContainer.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import "./NotificationContainer.css";
+import { connect } from 'react-redux';
+
+class NotificationContainer extends Component {
+    render() {
+        return (
+            <div className="notification">
+
+            </div>
+        )
+    }
+}
+
+// Map State To Props
+const mapStateToProps = (reduxState) => ({
+    //notification: reduxState.notification
+})
+
+// Export connected component
+export default connect(mapStateToProps)(NotificationContainer);
