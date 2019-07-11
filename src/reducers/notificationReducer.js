1eee5f00e6d6008d39fd8915dec840473ad24047
Billy Vlachos <williamcaminiti@live.com>
2019-07-11 23:14:24 +0200

    Update the reducer to listen to the action types

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,7 +1,12 @@
+import { SET_NOTIFICATION, REMOVE_NOTIFICATION } from './../actions/notificationActions';
 
 // Define the notification reducer
 const reducer = (state = null, action = {}) => {
     switch (action.type) {
+        case SET_NOTIFICATION:
+            return action.payload;
+        case REMOVE_NOTIFICATION:
+            return null;
         default:
             return state;
     }
