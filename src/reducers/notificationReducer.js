070086fe23b79f17502e3f958d8203ec69c53258
Billy Vlachos <williamcaminiti@live.com>
2019-07-11 23:08:28 +0200

    Add notification reducer

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+
+// Define the notification reducer
+const reducer = (state = null, action = {}) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+// Export the reducer
+export default reducer;
