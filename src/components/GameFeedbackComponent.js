dc8f5a527ece14599169f67aaa183be3cee36edf
coderhook <abel1987@hotmail.es>
2019-07-11 11:34:18 +0200

    Initialize GmeFeedBak component

---
+++ src/components/GameFeedbackComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './css/GameFeedbackComponent.css'
+
+export default class GameFeedback extends Component {
+  render() {
+    return (
+      <div className="game-feedback">
+        
+      </div>
+    )
+  }
+}
