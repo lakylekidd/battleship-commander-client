8c480aeb0b7ba823887529bcadc24662f616042d
coderhook <abel1987@hotmail.es>
2019-07-12 12:22:53 +0200

    Add comment

--- src/components/Board/TileComponent.jsx
+++ src/components/Board/TileComponent.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import "./TileComponent.css";
 
-export default class TileComponent extends Component {
+class TileComponent extends Component {
+    handleClick = () => {
+        this.props.onClick
+
+    }
+
+    // Pass in from parent if this is configuration board or not
+    // if configuring then check your board  and get tile idx
+    // if not configuring check if this is enemy board.. if so, dont show boats only hits
+    // if not enemy board, show ships and hits
+
     render() {
         return (
-            <div onClick={this.props.onClick} className="tile">
+            <div onClick={this.handleClick} className={`tile ${ }`}>
                 <span className="label">
                     x:{this.props.posX} y:{this.props.posY} i:{this.props.index}
                 </span>
@@ -13,3 +24,14 @@ export default class TileComponent extends Component {
         )
     }
 }
+
+// Map state to props
+const mapStateToProps = (reduxStore) => {
+    return {
+        currentGame: reduxStore.currentGame,
+        currentUser: reduxStore.currentUser
+    }
+}
+
+// Export the connected component
+export default connect(mapStateToProps, null)(TileComponent);
